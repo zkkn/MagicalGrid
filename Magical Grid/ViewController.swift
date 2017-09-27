4616ea4295f646785a910bb8a17533d848f9be71
Shoichi Kanzaki <zaksho1121@gmail.com>
2017-09-28 00:25:50 +0900

    セルを押したら浮かび上がり、時間の経過でもとに戻っていくアニメーション実装

--- Magical Grid/ViewController.swift
+++ Magical Grid/ViewController.swift
@@ -35,6 +35,8 @@ class ViewController: UIViewController {
         view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan)))
     }
     
+    var selectedCell: UIView?
+    
     func handlePan(gesture: UIPanGestureRecognizer) {
         let location = gesture.location(in: view)
         
@@ -45,8 +47,31 @@ class ViewController: UIViewController {
         print(i,j)
         
         let key = "\(i)|\(j)"
-        let cellView = cells[key]
-        cellView?.backgroundColor = .white
+        guard let cellView = cells[key] else { return }
+        
+        if selectedCell != cellView {
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1,
+                           options: .curveEaseOut, animations: {
+                            self.selectedCell?.layer.transform = CATransform3DIdentity
+            }, completion: nil)
+        }
+        
+        selectedCell = cellView
+        
+        view.bringSubview(toFront: cellView)
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1,
+                       options: .curveEaseOut, animations: {
+                        cellView.layer.transform = CATransform3DMakeScale(3, 3, 3)
+        }, completion: nil)
+        
+        if gesture.state == .ended {
+            UIView.animate(withDuration: 0.5, delay: 0.25, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5,
+                           options: .curveEaseOut, animations: {
+                            cellView.layer.transform = CATransform3DIdentity
+            }, completion: { (_) in
+            })
+        }
         
 //        var loopCount = 0
 //        for subview in view.subviews {
