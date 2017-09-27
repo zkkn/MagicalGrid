1827c309e6ad89075a875c9e0afaa666a9fc89d5
Shoichi Kanzaki <zaksho1121@gmail.com>
2017-09-27 22:18:17 +0900

    by gesturing, the grid's color turns to white

--- Magical Grid/ViewController.swift
+++ Magical Grid/ViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let numViewPerRow = 15
+    var cells = [String: UIView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let numViewPerRow = 15
         let width = view.frame.width / CGFloat(numViewPerRow)
         
         for j in 0...30 {
@@ -24,6 +26,9 @@ class ViewController: UIViewController {
                 cellView.layer.borderWidth = 0.5
                 cellView.layer.borderColor = UIColor.black.cgColor
                 view.addSubview(cellView)
+                
+                let key = "\(i)|\(j)"
+                cells[key] = cellView
             }
         }
         
@@ -32,7 +37,25 @@ class ViewController: UIViewController {
     
     func handlePan(gesture: UIPanGestureRecognizer) {
         let location = gesture.location(in: view)
-        print(location)
+        
+        let width = view.frame.width / CGFloat(numViewPerRow)
+        
+        let i = Int(location.x / width)
+        let j = Int(location.y / width)
+        print(i,j)
+        
+        let key = "\(i)|\(j)"
+        let cellView = cells[key]
+        cellView?.backgroundColor = .white
+        
+//        var loopCount = 0
+//        for subview in view.subviews {
+//            if subview.frame.contains(location) {
+//                subview.backgroundColor = .black
+//                print("loopCopunt:", loopCount)
+//            }
+//            loopCount += 1
+//        }
     }
     
     fileprivate func randomColor() -> UIColor {
